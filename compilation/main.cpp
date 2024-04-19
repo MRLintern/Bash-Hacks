05ede7329f9fb760fed6c25b39fbb5a8019c83e6
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-04-19 17:11:45 +0100

    Create main.cpp

---
+++ compilation/main.cpp
@@ -0,0 +1,19 @@
+// source code
+
+#include "add.h"
+#include <iostream>
+
+int main() {
+
+  std::cout<<"The sum of 12 and 5 is "<<add(15,5)<<"\n";
+
+}
+
+// function
+
+int add(int x, int y) {
+
+  return x + y;
+
+}
+
