5e5cba59b7cde423039d2ec69396d3bbf26e6693
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-04-20 17:17:26 +0100

    new source file

--- compilation/main.cpp
+++ compilation/main.cpp
@@ -1,19 +1,29 @@
-// source code
+// adds 2 integers
 
 #include "add.h"
 #include <iostream>
 
 int main() {
 
-  std::cout<<"The sum of 12 and 5 is "<<add(15,5)<<"\n";
+	// user provide 2 integers
+
+	int x;
+	int y;
+
+	std::cout<<"Enter an integer: "; std::cin>>x;
+	std::cout<<"\n";
+	std::cout<<"Enter another integer: "; std::cin>>y;
+	std::cout<<"\n";
+
+	// call function to add 2 ints
+
+	std::cout<<"The sum of "<<x<<" and "<<y<<" is "<<add(x,y)<<"\n";
 
 }
 
-// function
+// add function
 
 int add(int x, int y) {
 
-  return x + y;
-
+	return x + y;
 }
-
