8493faf258725fb8d7530a86af3fed6f1d069ecb
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-04-22 16:08:11 +0100

    iupdated source/driver code

--- extraction/FileExtract/main.cpp
+++ extraction/FileExtract/main.cpp
@@ -1,1 +1,30 @@
 
+// adds 2 integers
+
+#include "add.h"
+#include <iostream>
+
+int main() {
+
+	// user provide 2 integers
+
+	int x;
+	int y;
+
+	std::cout<<"Enter an integer: "; std::cin>>x;
+	std::cout<<"\n";
+	std::cout<<"Enter another integer: "; std::cin>>y;
+	std::cout<<"\n";
+
+	// call function to add 2 ints
+
+	std::cout<<"The sum of "<<x<<" and "<<y<<" is "<<add(x,y)<<"\n";
+
+}
+
+// add function
+
+int add(int x, int y) {
+
+	return x + y;
+}
